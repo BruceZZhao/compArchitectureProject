9d73452d0bf510099d34ad8a5653c3fd3ac988c3
BruceZZhao <brucezzhao@163.com>
2024-11-12 16:15:32 +0000

    Update maze0.asm

--- maze0.asm
+++ maze0.asm
@@ -1,7 +1,7 @@
 .data
 myarray: .ascii "0000,0000,0000,0000,0000,0000,0000,1000,0110,1000,1110,0011,0000,1100,1111,0010,0101,0100,0000,0001,1001,1110,0011,0001,0000,1100,1110,0111,1000,0111,0000,0001,0101,1001,1110,0011,1000,1000,1011,0010,1001,0010"
-# Each cell contains 4 digits of data. The 4 digits stand for 4 directions, which are "Forward,Right,Back,Left" respectively, where "0" stands for wall, "1" stands for valid path#
-# All cells in the first row and first column are considered outside the maze, and are filled with data "0000", except for the starting point.
+# Each cell contains 4 digits of number. The 4 digits stand for 4 directions, which are "Forward,Right,Back,Left" respectively, where "0" stands for wall, "1" stands for valid path#
+# All cells in the first row and first column are considered outside the maze, and are filled with number "0000", except for the starting point.
 
 mazewidth: .word 6
 ycord: .word 1 # Can change this. Input ranges from 0-6 (7 rows in total).
